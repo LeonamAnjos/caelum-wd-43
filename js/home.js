cee70d1be32e17ce52034ba9e07df6681bc9332a
Leonam Anjos <leonam.ricardo@gmail.com>
2014-12-02 08:00:43 -0200

    Capítulos 05 e 06

---
+++ js/home.js
@@ -0,0 +1,31 @@
+// associação da função com o evento
+document.querySelector('#form-busca').onsubmit = function() {
+	if (document.querySelector('#q').value == '') {
+		document.querySelector('#q').style.background = 'red';
+		return false;
+	}
+};
+
+
+var banners = ['img/destaque-home.png', 'img/destaque-home-2.png'];
+var bannerAtual = 0;
+
+function trocaBanner() {
+	bannerAtual = (bannerAtual + 1) % 2;
+	document.querySelector('.destaque img').src = banners[bannerAtual];
+}
+
+var timer = setInterval(trocaBanner, 2000);
+
+var controle = document.querySelector('.pause');
+controle.onclick = function() {
+  if (controle.className == 'pause') {
+    clearInterval(timer);
+    controle.className = 'play';
+  } else {
+    timer = setInterval(trocaBanner, 2000);
+    controle.className = 'pause';
+  }
+
+  return false;
+};
